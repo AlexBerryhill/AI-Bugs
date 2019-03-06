69148c3ea10510e69f51ce4ca35a2489732efd02
Alexander Berryhill <47376347+AlexBerryhill@users.noreply.github.com>
2019-03-05 19:15:11 -0700

    Add files via upload

---
+++ Bugs_Ai_Main.java
@@ -0,0 +1,37 @@
+// We need a world to work on.
+AiWorld world;
+
+/******************************************************
+ * Setup our program
+ ******************************************************/
+void setup () 
+{
+  // This is the size of our screen
+  size(1000,700);
+  // We will slow things down a bit
+  frameRate(10);
+  
+  // Initialize our world
+  world = new AiWorld();
+  // Put some ants into our world
+  for (int i = 0; i < 100; i++)
+    world.addAnt();
+}
+
+/******************************************************
+ * Main loop for our program to run
+ ******************************************************/
+void draw() {
+  // This will be a dark gray background (one variable)
+  background(50);
+  
+  // Check for next generation
+  if (world.isDead()) {
+    world.reproduce();
+    world.mutate();
+  }
+ 
+  // Update and draw our ants
+  world.update();
+  world.draw();
+}
